03e5c2a25cc22e9d6a6494b5e7f4c7436b92b54d
Luc Lucens <luclucens+gitlab@gmail.com>
2017-12-15 12:56:32 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const crypto = require('crypto')
+const fs = require('fs')
+const prompt = require('prompt')
+const colors = require('colors/safe')
+const qr = require('qr-image')
+const algorithm = 'aes-256-ofb'
+
+prompt.message = ('')
+prompt.start()
+
+const encryptSeed = (seed, password) => {
+  const cipher = crypto.createCipher(algorithm, password)
+  let cryptedSeed = cipher.update(seed, 'utf8', 'hex')
+  cryptedSeed += cipher.final('hex')
+  console.log(colors.grey('encrypted seed🔒:  '), cryptedSeed)
+  const newQR = qr.image(cryptedSeed, { type: 'svg' })
+  if (!fs.existsSync('./QR_image')) {
+    fs.mkdirSync('./QR_image')
+  }
+  newQR.pipe(fs.createWriteStream('./QR_image/cryptedSeedQR.svg'))
+  return cryptedSeed
+}
+
+const decryptSeed = (cryptedSeed, password) => {
+  const decipher = crypto.createDecipher(algorithm, password)
+  let decipheredSeed = decipher.update(cryptedSeed, 'hex', 'utf8')
+  decipheredSeed += decipher.final('utf8')
+  console.log(colors.grey('decrypted seed🔓:  '), decipheredSeed)
+  return decipheredSeed
+}
+
+prompt.get([{
+  name: 'taskType',
+  description: 'encrypt or decrypt?',
+  type: 'string',
+  required: true
+}], (err, result) => {
+  if (err) {
+    console.error(err)
+  } else {
+    if (result.taskType === 'encrypt') {
+      prompt.get([{
+        name: 'seed',
+        type: 'string',
+        pattern: /^[A-Z9]{81}$/,
+        message: 'Seed must be an 81 character random assortment of A-Z and 9',
+        required: true
+      }, {
+        name: 'password',
+        type: 'string',
+        required: true
+      }], (err, result) => {
+        if (err) {
+          console.error(err)
+        } else {
+          encryptSeed(result.seed, result.password)
+        }
+      })
+    } else if (result.taskType === 'decrypt') {
+      prompt.get([{
+        name: 'cryptedSeed',
+        type: 'string',
+        required: true
+      }, {
+        name: 'password',
+        type: 'string',
+        required: true
+      }], (err, result) => {
+        if (err) {
+          console.error(err)
+        } else {
+          decryptSeed(result.cryptedSeed, result.password)
+        }
+      })
+    } else {
+      console.log('nooooo')
+    }
+  }
+})
+
+// 'GJNASZSJCKSDKTUBKVSWCYGWZWEFB9LFLSZVKNBIPYSHYHAAGECTZDNHJOGNUPYVJCWBWUOLZHDMRPNOU'
